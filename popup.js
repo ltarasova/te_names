ae50361d5d97e013b96e7abb69e436a12e594d52
ltarasova <31202590+ltarasova@users.noreply.github.com>
2019-01-12 17:00:44 +0700

    add files

---
+++ popup.js
@@ -0,0 +1,16 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+'use strict';
+
+let changeColor = document.getElementById('changeColor');
+
+changeColor.onclick = function(element) {
+    let color = "#3aa757";
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+      chrome.tabs.executeScript(
+          tabs[0].id,
+		  {code: 'document.getElementById('reservationStepFirstName').value = "FOMA";'});
+    });
+  };

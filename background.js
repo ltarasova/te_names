bf3c80f21e5b2d0590f6702b333e9a075ff16f78
l.tarasova <l.tarasova@eastbanctech.ru>
2019-01-13 22:43:16 +0700

    Ограничение для урлов starts with hrdt

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
       chrome.declarativeContent.onPageChanged.addRules([{
         conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'hrdt-stage.eastbanctech.ru'},
+          pageUrl: {hostPrefix: 'hrdt'},
         })
         ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
